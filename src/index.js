446591107862c9bb8454a8be65f5c3770fdf76c1
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2022-07-28 22:22:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let res = [];
+  let letters = [];
+  let pos = 0;
+
+  while(pos < expr.length) {
+    letters.push(expr.substring(pos, pos = pos + 10))
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    let letter = [];
+    pos = 0;
+
+    if(letters[i]=='**********'){
+      res.push(' ');
+      continue;
+    }
+
+    while(pos < letters[i].length) {
+    letter.push(letters[i].substring(pos, pos = pos + 2))
+    }
+
+    letter = letter.filter(i => i!=='00').map(i => (i == '10')? i='.': i='-').join('');
+    res.push(MORSE_TABLE[letter])
+  }
+  return res.join('');
 }
 
 module.exports = {
